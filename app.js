49665d7f408b25c288c37c6200c18497c44d63c6
ChristopherNl <christophernoel651@gmail.com>
2026-06-11 12:53:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("app.js démarre");
+
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
